aa9d22a8fc03692be6773f8eb187585c7d7af9a7
Papershaper <papershaper@gmail.com>
2022-11-12 11:40:44 +0100

    Blink added

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define LED_BUILTIN 2
+ 
+void setup()
+{
+  // Initialize LED pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
+ 
+void loop()
+{
+  // Set the LED HIGH
+  digitalWrite(LED_BUILTIN, HIGH);
+ 
+  // Wait for a second
+  delay(1000);
+ 
+  // Set the LED LOW
+  digitalWrite(LED_BUILTIN, LOW);
+ 
+   // Wait for a second
+  delay(1000);
 }
